09bafbae48ee242f2a5d197ff31fd1614052514f
christian williams <christian.williamsj@gmail.com>
2019-09-24 07:08:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,7 +3,7 @@ class Transfer
   attr_accessor :sender, :receiver, :amount, :status, :count 
   
   def initialize(sender, receiver, amount)
-    @name = sender 
+    @sender = sender 
     @receiver = receiver
     @amount = amount 
     @status = "pending"
