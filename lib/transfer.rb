35fea02dafa71eff2872c000e0f70bca033ed1a0
christian williams <christian.williamsj@gmail.com>
2019-09-24 07:21:42 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,6 +24,7 @@ class Transfer
     
     elsif @status == "complete"
     puts "Transaction was already executed"
+  else
     @sender.deposit( @amount * -1 )
     @receiver.deposit( @amount )
     @status = "complete"
