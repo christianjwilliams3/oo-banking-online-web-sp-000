7f124a6401f65d651f97a011dd2d07b35123f4f4
christian williams <christian.williamsj@gmail.com>
2019-09-24 07:28:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,16 +20,16 @@ class Transfer
   def execute_transaction
     if @sender.balance < @amount
       @status = "rejected"
-    return "Transaction rejected. Please check your account balance."
-    
-    elsif @status == "complete"
-       puts "Transaction was already executed"
+      return "Transaction rejected. Please check your account balance."
+
+     elsif @status == "complete"
+      puts "Transaction was already excuted"
     else
-      @sender.deposit ( @amount * -1 )
-      @receiver.deposit ( @amount )
+      @sender.deposit( @amount * -1 ) 
+      @receiver.deposit( @amount )
       @status = "complete"
     end
-  end 
+  end
 
   def reverse_transfer
     if @status == "complete"
